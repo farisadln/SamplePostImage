009a3a3032a2de9b8ed79104f3c3b2cd5943870c
farisdx <farisdx66@gmail.com>
2018-12-16 17:58:53 +0700

    fix storage

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyDTQiK9DRt08OJ7j6ze3S1v-eKKiHn2dDA',
-    authDomain: 'imagepost-c93a8.firebaseapp.com',
-    databaseURL: 'https://imagepost-c93a8.firebaseio.com',
-    projectId: 'imagepost-c93a8',
-    storageBucket: 'imagepost-c93a8.appspot.com',
-    messagingSenderId: '191872754600'
+    apiKey: 'AIzaSyDeRtoRpcekKUD-dWf1oAacWBCN--M3-C0',
+    authDomain: 'postimage-a783b.firebaseapp.com',
+    databaseURL: 'https://postimage-a783b.firebaseio.com',
+    projectId: 'postimage-a783b',
+    storageBucket: 'postimage-a783b.appspot.com',
+    messagingSenderId: '597101010958'
   }
 };
