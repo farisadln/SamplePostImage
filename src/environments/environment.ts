d106fcdc2e4083e8cc0cc833e4c70d0e40f1dffb
farisdx <farisdx66@gmail.com>
2018-12-24 19:18:05 +0700

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyDeRtoRpcekKUD-dWf1oAacWBCN--M3-C0',
-    authDomain: 'postimage-a783b.firebaseapp.com',
-    databaseURL: 'https://postimage-a783b.firebaseio.com',
-    projectId: 'postimage-a783b',
-    storageBucket: 'postimage-a783b.appspot.com',
-    messagingSenderId: '597101010958'
+    apiKey: 'x',
+    authDomain: 'x',
+    databaseURL: 'x',
+    projectId: 'x',
+    storageBucket: 'px',
+    messagingSenderId: '52x2378'
   }
 };
